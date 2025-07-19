968e0662e7465278e196e7f81141ace36f34760c
pape sembene <sembenpape4@gmail.com>
2025-07-19 16:33:14 +0000

    sur le point de finaliser avec seeders

--- seeders/seeder.php
+++ seeders/seeder.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 $driver = $_ENV['DB_DRIVER'];
